909a669049f1c15a760c5e38eed804e8afe1fa83
daehy <rkdeown10@naver.com>
2023-01-26 05:48:58 +0900

    first commit!

--- GooseGooseDuck/Form1.Designer.cs
+++ GooseGooseDuck/Form1.Designer.cs
@@ -36,12 +36,15 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.label5 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,7 +56,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(317, 15);
+            this.label1.Location = new System.Drawing.Point(223, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(60, 15);
             this.label1.TabIndex = 2;
@@ -63,7 +66,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(317, 40);
+            this.label2.Location = new System.Drawing.Point(223, 40);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 15);
             this.label2.TabIndex = 3;
@@ -72,7 +75,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(212, 12);
+            this.checkBox1.Location = new System.Drawing.Point(118, 12);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(84, 19);
             this.checkBox1.TabIndex = 4;
@@ -83,7 +86,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(317, 67);
+            this.label3.Location = new System.Drawing.Point(223, 67);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 15);
             this.label3.TabIndex = 5;
@@ -92,7 +95,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(317, 93);
+            this.label4.Location = new System.Drawing.Point(223, 93);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 15);
             this.label4.TabIndex = 6;
@@ -100,7 +103,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(212, 59);
+            this.button2.Location = new System.Drawing.Point(118, 90);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(88, 27);
             this.button2.TabIndex = 7;
@@ -108,26 +111,16 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(16, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 104);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "GetProsess and Setting";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(106, 59);
+            this.textBox1.Location = new System.Drawing.Point(12, 90);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 23);
             this.textBox1.TabIndex = 8;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(106, 93);
+            this.textBox2.Location = new System.Drawing.Point(12, 124);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 23);
             this.textBox2.TabIndex = 9;
@@ -135,7 +128,7 @@
             // button3
             // 
             this.button3.AccessibleName = "";
-            this.button3.Location = new System.Drawing.Point(212, 92);
+            this.button3.Location = new System.Drawing.Point(118, 123);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(88, 23);
             this.button3.TabIndex = 10;
@@ -145,29 +138,75 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(106, 12);
+            this.trackBar1.Location = new System.Drawing.Point(12, 12);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Minimum = 1;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(100, 45);
             this.trackBar1.TabIndex = 12;
-            this.trackBar1.Value = 1;
+            this.trackBar1.Value = 10;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(212, 34);
+            this.label5.Location = new System.Drawing.Point(118, 34);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(38, 15);
             this.label5.TabIndex = 13;
             this.label5.Text = "speed";
             // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 63);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(50, 19);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "벽뚫";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 166);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(118, 19);
+            this.checkBox3.TabIndex = 16;
+            this.checkBox3.Text = "모든 능력 쿨타임";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 191);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(106, 19);
+            this.checkBox4.TabIndex = 17;
+            this.checkBox4.Text = "오리 킬 쿨타임";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 124);
+            this.ClientSize = new System.Drawing.Size(379, 241);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button3);
@@ -179,7 +218,6 @@
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -197,11 +235,14 @@
         private Label label3;
         private Label label4;
         private Button button2;
-        private Button button1;
         private TextBox textBox1;
         private TextBox textBox2;
         private Button button3;
         private TrackBar trackBar1;
         private Label label5;
+        private CheckBox checkBox2;
+        private Button button1;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
     }
 }
--- GooseGooseDuck/Form1.cs
+++ GooseGooseDuck/Form1.cs
@@ -17,7 +17,6 @@ namespace GooseGooseDuck
         GetMemory gm = new GetMemory();
         float old_position_x = 0, old_position_y = 0;
         ulong XYhookDump;
-
         string xarr = "01ACA7C0";
         string xarroffset1 = "48";
         string xarroffset2 = "370";
@@ -26,14 +25,33 @@ namespace GooseGooseDuck
         string xarroffset5 = "2C";
 
 
+        ulong SpeedDump;
+        string speedarr = "03C22000";
+        string Speedoffset1 = "80";
+        string Speedoffset2 = "80";
+        string Speedoffset3 = "20";
+        string Speedoffset4 = "B8";
+        string Speedoffset5 = "270";
+
+        ulong spaceDump;
+        string spacearr = "03C814B0";
+        string spaceoffset1 = "40";
+        string spaceoffset2 = "B8";
+        string spaceoffset3 = "0";
+        string spaceoffset4 = "20";
+        string spaceoffset5 = "1B0";
+
+        ulong killDump;
+        string killarr = "03C814B0";
+        string killoffset1 = "40";
+        string killoffset2 = "B8";
+        string killoffset3 = "0";
+        string killoffset4 = "20";
+        string killoffset5 = "250";
+
+
         float current_position_x, current_position_y;
 
-        [DllImport("user32.dll")]
-        private static extern int RegisterHotKey(int hwnd, int id, int fsModifiers, int vk);
-
-        //ÇÖÅ°Á¦°Å
-        [DllImport("user32.dll")]
-        private static extern int UnregisterHotKey(int hwnd, int id);
 
         public Form1()
         {
@@ -42,12 +60,27 @@ namespace GooseGooseDuck
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            gm.get_handel();
+            xyset();
+            speedset();
+            spaceset();
+            killset();
 
+            label5.Text = trackBar1.Value.ToString();
+            timer1.Enabled = true;
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            gm.get_handel();
+
+        private void speedset() {
+            SpeedDump = gm.get_memory_ulong(gm.GameAssembly + Convert.ToUInt64(speedarr, 16), 8);
+            SpeedDump = gm.get_memory_ulong(SpeedDump + Convert.ToUInt64(Speedoffset1, 16), 8);
+            SpeedDump = gm.get_memory_ulong(SpeedDump + Convert.ToUInt64(Speedoffset2, 16), 8);
+            SpeedDump = gm.get_memory_ulong(SpeedDump + Convert.ToUInt64(Speedoffset3, 16), 8);
+            SpeedDump = gm.get_memory_ulong(SpeedDump + Convert.ToUInt64(Speedoffset4, 16), 8);
+            SpeedDump = SpeedDump + Convert.ToUInt64(Speedoffset5, 16);
+        }
+
+        private void xyset() {
             XYhookDump = gm.get_memory_ulong(gm.UnityPlayer + Convert.ToUInt64(xarr, 16), 8);
             XYhookDump = gm.get_memory_ulong(XYhookDump + Convert.ToUInt64(xarroffset1, 16), 8);
             XYhookDump = gm.get_memory_ulong(XYhookDump + Convert.ToUInt64(xarroffset2, 16), 8);
@@ -60,13 +93,65 @@ namespace GooseGooseDuck
             label2.Text = current_position_y.ToString();
             label3.Text = "old_X = " + current_position_x.ToString();
             label4.Text = "old_Y = " + current_position_y.ToString();
-
             old_position_x = current_position_x;
             old_position_y = current_position_y;
+        }
 
+        private void spaceset() {
+            spaceDump = gm.get_memory_ulong(gm.GameAssembly + Convert.ToUInt64(spacearr, 16), 8);
+            spaceDump = gm.get_memory_ulong(spaceDump + Convert.ToUInt64(spaceoffset1, 16), 8);
+            spaceDump = gm.get_memory_ulong(spaceDump + Convert.ToUInt64(spaceoffset2, 16), 8);
+            spaceDump = gm.get_memory_ulong(spaceDump + Convert.ToUInt64(spaceoffset3, 16), 8);
+            spaceDump = gm.get_memory_ulong(spaceDump + Convert.ToUInt64(spaceoffset4, 16), 8);
+            spaceDump = spaceDump + Convert.ToUInt64(spaceoffset5, 16);
+        }
+
+        private void killset()
+        {
+            killDump = gm.get_memory_ulong(gm.GameAssembly + Convert.ToUInt64(killarr, 16), 8);
+            killDump = gm.get_memory_ulong(killDump + Convert.ToUInt64(killoffset1, 16), 8);
+            killDump = gm.get_memory_ulong(killDump + Convert.ToUInt64(killoffset2, 16), 8);
+            killDump = gm.get_memory_ulong(killDump + Convert.ToUInt64(killoffset3, 16), 8);
+            killDump = gm.get_memory_ulong(killDump + Convert.ToUInt64(killoffset4, 16), 8);
+            killDump = killDump + Convert.ToUInt64(killoffset5, 16);
+        }
+
+        private void move() {
+            if (current_position_x != old_position_x)
+            {
+                if (current_position_x - old_position_x > 0)
+                {
+                    gm.put_memory(XYhookDump + Convert.ToUInt64(xarroffset5, 16), BitConverter.GetBytes(Convert.ToSingle(current_position_x + 1.5)));
+                }
+                else if (current_position_x - old_position_x < 0)
+                {
+                    gm.put_memory(XYhookDump + Convert.ToUInt64(xarroffset5, 16), BitConverter.GetBytes(Convert.ToSingle(current_position_x - 1.5)));
+                }
+
+            }
+            if (current_position_y != old_position_y)
+            {
+                if (current_position_y - old_position_y > 0)
+                {
+                    gm.put_memory(XYhookDump + Convert.ToUInt64(xarroffset5, 16) + 4, BitConverter.GetBytes(Convert.ToSingle(current_position_y + 1.5)));
+                }
+                else if (current_position_y - old_position_y < 0)
+                {
+                    gm.put_memory(XYhookDump + Convert.ToUInt64(xarroffset5, 16) + 4, BitConverter.GetBytes(Convert.ToSingle(current_position_y - 1.5)));
+                }
+
+            }
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gm.get_handel();
+            xyset();
+            speedset();
+            spaceset();
             timer1.Enabled = true;
-
-            label5.Text = (trackBar1.Value * 0.01).ToString();
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -74,37 +159,28 @@ namespace GooseGooseDuck
             current_position_x = gm.get_memory_float(XYhookDump + Convert.ToUInt64(xarroffset5, 16), 4);
             current_position_y = gm.get_memory_float(XYhookDump + Convert.ToUInt64(xarroffset5, 16) + 4, 4);
 
-            label1.Text = current_position_x.ToString();
-            label2.Text = current_position_y.ToString();
+            label1.Text = "X = " + current_position_x.ToString();
+            label2.Text = "Y = " + current_position_y.ToString();
             label3.Text = "old_X = " + old_position_x.ToString();
             label4.Text = "old_Y = " + old_position_y.ToString();
-            if (checkBox1.Checked == true)
+
+            if (checkBox1.Checked == true) 
             {
-                if (current_position_x != old_position_x)
-                {
-                    if (current_position_x - old_position_x > 0)
-                    {
-                        gm.put_memory(XYhookDump + Convert.ToUInt64(xarroffset5, 16), BitConverter.GetBytes(Convert.ToSingle(current_position_x + (Convert.ToSingle(label5.Text)))));
-                    }
-                    else if (current_position_x - old_position_x < 0)
-                    {
-                        gm.put_memory(XYhookDump + Convert.ToUInt64(xarroffset5, 16), BitConverter.GetBytes(Convert.ToSingle(current_position_x - (Convert.ToSingle(label5.Text)))));
-                    }
-
-                }
-                if (current_position_y != old_position_y)
-                {
-                    if (current_position_y - old_position_y > 0)
-                    {
-                        gm.put_memory(XYhookDump + Convert.ToUInt64(xarroffset5, 16) + 4, BitConverter.GetBytes(Convert.ToSingle(current_position_y + (Convert.ToSingle(label5.Text)))));
-                    }
-                    else if (current_position_y - old_position_y < 0)
-                    {
-                        gm.put_memory(XYhookDump + Convert.ToUInt64(xarroffset5, 16) + 4, BitConverter.GetBytes(Convert.ToSingle(current_position_y - (Convert.ToSingle(label5.Text)))));
-                    }
-
-                }
+                gm.put_memory(SpeedDump, BitConverter.GetBytes(Convert.ToSingle(trackBar1.Value)));
             }
+
+            if (checkBox2.Checked == true) {
+                move();
+            }
+
+            if (checkBox3.Checked == true) {
+                gm.put_memory(spaceDump, BitConverter.GetBytes(Convert.ToSingle(0)));
+            }
+            if (checkBox4.Checked == true)
+            {
+                gm.put_memory(killDump, BitConverter.GetBytes(Convert.ToSingle(0)));
+            }
+
             current_position_x = gm.get_memory_float(XYhookDump + Convert.ToUInt64(xarroffset5, 16), 4);
             current_position_y = gm.get_memory_float(XYhookDump + Convert.ToUInt64(xarroffset5, 16) + 4, 4);
             old_position_x = current_position_x;
@@ -134,13 +210,33 @@ namespace GooseGooseDuck
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            label5.Text = (trackBar1.Value * 0.01).ToString();
+            label5.Text = trackBar1.Value.ToString();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            gm.get_handel();
+            xyset();
+            speedset();
+            spaceset();
+            killset();
+            timer1.Enabled = true;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = label1.Text;
-            textBox2.Text = label2.Text;
+            textBox1.Text = current_position_x.ToString();
+            textBox2.Text = current_position_y.ToString();
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
